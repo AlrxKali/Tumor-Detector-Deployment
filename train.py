cfb99d84af4d9d7b9c3fa08e52ad1444c112c940
alrxkali <accual1013@gmail.com>
2023-01-21 18:18:44 -0500

    model saved

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-
+import mlflow
+import mlflow.sklearn
+import mlflow.keras
 from PIL import Image as im
+
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
-
 from keras.applications.vgg16 import VGG16
 from keras import layers
 from keras.models import Sequential
@@ -13,6 +15,8 @@ from keras.optimizers import RMSprop
 from keras.callbacks import EarlyStopping
 
 from utils import load_data, crop_imgs, save_new_images, preprocess_imgs, build_data_pipelines
+
+mlflow.set_experiment('avasure_classification_model')
 
 def train():
     TRAIN_DIR = 'Data/TRAIN/'
@@ -38,60 +42,66 @@ def train():
     X_train_prep = preprocess_imgs(set_name=X_train_crop, img_size=IMG_SIZE)
     X_test_prep = preprocess_imgs(set_name=X_test_crop, img_size=IMG_SIZE)
     X_val_prep = preprocess_imgs(set_name=X_val_crop, img_size=IMG_SIZE)
+    
+    with mlflow.start_run(run_name='vgg16_model_experiment') as run:
+        train_generator, val_generator = build_data_pipelines(IMG_SIZE)
 
-    train_generator, val_generator = build_data_pipelines(IMG_SIZE)
+        # load base model
+        vgg16_weight_path = 'Weights/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        base_model = VGG16(
+            weights=vgg16_weight_path,
+            include_top=False, 
+            input_shape=IMG_SIZE + (3,)
+        )
 
-    # load base model
-    vgg16_weight_path = 'Weights/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
-    base_model = VGG16(
-        weights=vgg16_weight_path,
-        include_top=False, 
-        input_shape=IMG_SIZE + (3,)
-    )
+        NUM_CLASSES = 1
 
-    NUM_CLASSES = 1
+        model = Sequential()
+        model.add(base_model)
+        model.add(layers.Flatten())
+        model.add(layers.Dropout(0.5))
+        model.add(layers.Dense(NUM_CLASSES, activation='sigmoid'))
 
-    model = Sequential()
-    model.add(base_model)
-    model.add(layers.Flatten())
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(NUM_CLASSES, activation='sigmoid'))
+        model.layers[0].trainable = False
 
-    model.layers[0].trainable = False
+        model.compile(
+            loss='binary_crossentropy',
+            optimizer=RMSprop(lr=1e-4),
+            metrics=['accuracy']
+        )
 
-    model.compile(
-        loss='binary_crossentropy',
-        optimizer=RMSprop(lr=1e-4),
-        metrics=['accuracy']
-    )
+        model.summary()
+        mlflow.keras.log_model(model, 'vgg16_model')
+        #mlflow.log_metric('Summary', model.summary())
 
-    model.summary()
+        EPOCHS = 30
+        es = EarlyStopping(
+            monitor='val_accuracy', 
+            mode='max',
+            patience=6
+        )
 
-    EPOCHS = 30
-    es = EarlyStopping(
-        monitor='val_accuracy', 
-        mode='max',
-        patience=6
-    )
+        history = model.fit_generator(
+            train_generator,
+            steps_per_epoch=50,
+            epochs=EPOCHS,
+            validation_data=val_generator,
+            validation_steps=25,
+            callbacks=[es]
+        )
 
-    history = model.fit_generator(
-        train_generator,
-        steps_per_epoch=50,
-        epochs=EPOCHS,
-        validation_data=val_generator,
-        validation_steps=25,
-        callbacks=[es]
-    )
+        predictions = model.predict(X_val_prep)
+        predictions = [1 if x>0.5 else 0 for x in predictions]
 
-    predictions = model.predict(X_val_prep)
-    predictions = [1 if x>0.5 else 0 for x in predictions]
+        accuracy = accuracy_score(y_val[:len(predictions)], predictions)
+        print('Val Accuracy = %.2f' % accuracy)
+        mlflow.log_metric('Accuracy', accuracy)
 
-    accuracy = accuracy_score(y_val[:len(predictions)], predictions)
-    print('Val Accuracy = %.2f' % accuracy)
+        confusion_mtx = confusion_matrix(y_val[:len(predictions)], predictions)
+        #mlflow.log_metric('Confusion Matrix', confusion_mtx)
 
-    confusion_mtx = confusion_matrix(y_val[:len(predictions)], predictions)
-
-    model.save('../Models/AvaSure_VGG_model.h5')
+        model.save('../Models/AvaSure_VGG_model.h5')
+        print('[...Model Saved...]')
 
 if __name__ == "__main__":
 
