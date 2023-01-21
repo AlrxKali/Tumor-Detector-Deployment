c407449dfa93494ae51d3f9b3b01ca34f34011be
alrxkali <accual1013@gmail.com>
2023-01-21 15:25:11 -0500

    model has accuracy of 80%

--- utils.py
+++ utils.py
@@ -1,8 +1,14 @@
 import numpy as np
 import os
-from tqdm import tqdm
 import cv2
+import os
+import imutils
 
+from tqdm import tqdm
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16, preprocess_input
+
+RANDOM_SEED = 123
 
 def load_data(dir_path, img_size=(100,100)):
     """
@@ -25,3 +31,101 @@ def load_data(dir_path, img_size=(100,100)):
     y = np.array(y)
     print(f'{len(X)} images loaded from {dir_path} directory.')
     return X, y, labels
+
+def crop_imgs(set_name, add_pixels_value=0):
+    """
+    Finds the extreme points on the image and crops the rectangular out of them
+    """
+    set_new = []
+    NoneType = type(None)
+
+    for img in set_name:
+        if type(img) != NoneType:
+            gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+            # threshold the image, then perform a series of erosions +
+            # dilations to remove any small regions of noise
+            thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.erode(thresh, None, iterations=2)
+            thresh = cv2.dilate(thresh, None, iterations=2)
+
+            # find contours in thresholded image, then grab the largest one
+            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+            c = max(cnts, key=cv2.contourArea)
+
+            # find the extreme points
+            extLeft = tuple(c[c[:, :, 0].argmin()][0])
+            extRight = tuple(c[c[:, :, 0].argmax()][0])
+            extTop = tuple(c[c[:, :, 1].argmin()][0])
+            extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+            ADD_PIXELS = add_pixels_value
+            new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS, extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+            set_new.append(new_img)
+
+    return np.array(set_new)
+
+def save_new_images(x_set, y_set, folder_name):
+    i = 0
+    for (img, imclass) in zip(x_set, y_set):
+        if imclass == 0:
+            cv2.imwrite(folder_name+'NO/'+str(i)+'.jpg', img)
+        else:
+            cv2.imwrite(folder_name+'YES/'+str(i)+'.jpg', img)
+        i += 1
+
+def preprocess_imgs(set_name, img_size):
+    """
+    Resize and apply VGG-15 preprocessing
+    """
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(
+            img,
+            dsize=img_size,
+            interpolation=cv2.INTER_CUBIC
+        )
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
+
+def build_data_pipelines(IMG_SIZE):
+
+    TRAIN_DIR = 'Data/TRAIN_CROP/'
+    VAL_DIR = 'Data/VAL_CROP/'
+
+    train_datagen = ImageDataGenerator(
+        rotation_range=15,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        shear_range=0.1,
+        brightness_range=[0.5, 1.5],
+        horizontal_flip=True,
+        vertical_flip=True,
+        preprocessing_function=preprocess_input
+    )
+
+    test_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input
+    )
+
+    train_generator = train_datagen.flow_from_directory(
+        TRAIN_DIR,
+        color_mode='rgb',
+        target_size=IMG_SIZE,
+        batch_size=32,
+        class_mode='binary',
+        seed=RANDOM_SEED
+    )
+
+    val_generator = test_datagen.flow_from_directory(
+        VAL_DIR,
+        color_mode='rgb',
+        target_size=IMG_SIZE,
+        batch_size=16,
+        class_mode='binary',
+        seed=RANDOM_SEED
+    )
+
+    return train_generator, val_generator
